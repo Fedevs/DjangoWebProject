297e4f11290411d6c1113552fd70f2ab6c93c29b
fedevs <fededevsit@gmail.com>
2021-06-21 17:35:33 -0300

    first commit

---
+++ WebApp/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+
+def home(request):
+    return HttpResponse("HOME")
+
+def services(request):
+    return HttpResponse("SERVICES")
+
+def shop(request):
+    return HttpResponse("SHOP")
+
+def blog(request):
+    return HttpResponse("BLOG")
+
+def contact(request):
+    message="HOLA"
+    return HttpResponse(message)
