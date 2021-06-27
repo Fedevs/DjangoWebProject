1d61de0ec584aa3de5825b80bd27183da22e3326
fedevs <fededevsit@gmail.com>
2021-06-27 19:21:03 -0300

    blog app working

--- Blog/models.py
+++ Blog/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Category(models.Model):
+    name=models.CharField(max_length=50)
+    created=models.DateTimeField(auto_now_add=True)
+    updated=models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
+
+class Post(models.Model):
+    title=models.CharField(max_length=50)
+    content=models.CharField(max_length=50, verbose_name="content")
+    image=models.ImageField(upload_to='Blog', null=True, blank=True)
+    author=models.ForeignKey(User, on_delete=models.CASCADE)
+    category=models.ManyToManyField(Category)
+    created=models.DateTimeField(auto_now_add=True)
+    updated=models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'post'
+        verbose_name_plural = 'posts'
+
+    def __str__(self):
+        return self.title
