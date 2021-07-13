192fe37dfd924bfb3e57695f98051aac8c76459f
fedevs <fededevsit@gmail.com>
2021-07-13 13:17:02 -0300

    shop working

--- WebProject/settings.py
+++ WebProject/settings.py
@@ -82,10 +82,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'djangoWebProject',
-        'USER': 'postgres',
-        'PASSWORD': '1234',
+        'USER': 'yourDBUser',
+        'PASSWORD': 'yourDBPw',
         'HOST': 'localhost',
-        'DATABASE_PORT': '5432',
+        'DATABASE_PORT': 'yourDBPort',
     }
 }
 
